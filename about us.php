fee33f4e97bc0d9e495990617d5103973b833f13
Jonar254 <jonathanmwangangi254@gmail.com>
2022-01-01 14:41:39 +0300

    Initial commit

---
+++ about us.php
@@ -0,0 +1,5 @@
+<?php
+echo ("We are Innovative Geeks
+A passionate team of designers,echo
+ developers and innovators dedicated 
+ to delivering solutions that help businesses work better.");
---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+    <strong>Copyright &copy; 2021 Brought To You By<a href="https://code-projects.org/">JONAR SOFWARE COMPANY ltd.</a></strong>
+</footer>
